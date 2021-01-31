5dd050a69c3dbebb5b836e9ef202a5de4b8b3368
andreyudi10 <andrianus.yudi@gmail.com>
2021-01-31 16:43:57 +0700

    looping

--- src/Components/PageTwoContent.jsx
+++ src/Components/PageTwoContent.jsx
@@ -1,353 +1,215 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { lighten, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
+import React, { useState, useEffect } from 'react'
+import ReactFlexyTable from 'react-flexy-table'
+import 'react-flexy-table/dist/index.css'
+import axios from 'axios';
+import deleteIcon from '../Components/logo192.png'
+import editIcon from '../Components/logo192.png'
 
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
-];
-
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function getComparator(order, orderBy) {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
-const headCells = [
-  { id: 'name', numeric: false, disablePadding: true, label: 'Dessert (100g serving)' },
-  { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-  { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-  { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-  { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
-  { id: 'new', numeric: true, disablePadding: false, label: 'new' },
-];
-
-function EnhancedTableHead(props) {
-  const { classes, onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } = props;
-  React.useEffect(()=>
-  console.log(props)
-  ,props)
-  const createSortHandler = (property) => (event) => {
-    onRequestSort(event, property);
-  };
-
-  return (
-    <TableHead>
-      <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{ 'aria-label': 'select all desserts' }}
-          />
-        </TableCell>
-        {headCells.map((headCell) => (
-          <TableCell
-            key={headCell.id}
-            align={headCell.numeric ? 'right' : 'left'}
-            padding={headCell.disablePadding ? 'none' : 'default'}
-            sortDirection={orderBy === headCell.id ? order : false}
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <span className={classes.visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </span>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
-
-EnhancedTableHead.propTypes = {
-  classes: PropTypes.object.isRequired,
-  numSelected: PropTypes.number.isRequired,
-  onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
-  orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
-};
-
-const useToolbarStyles = makeStyles((theme) => ({
-  root: {
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(1),
-  },
-  highlight:
-    theme.palette.type === 'light'
-      ? {
-          color: theme.palette.secondary.main,
-          backgroundColor: lighten(theme.palette.secondary.light, 0.85),
+const PageTwoContent = () => {
+  const [caseSensivite, setCaseSensivite] = useState(false)
+  const [sortable, setSortable] = useState(true)
+  const [filterable, setFilterable] = useState(true)
+  const [data, setData] = useState([])
+  const [downloadExcelProps, setDownloadExcelProps] = useState({
+    type: 'all',
+    title: 'test',
+    showLabel: true
+  })
+  useEffect(() => {
+    var axios = require('axios');    
+    var config = {
+        method: 'get',
+        url: 'https://laporan.vklaim.com/ci_laporan/870_laporan_sensus_igd?dept_prefix=C&jenis_periode=1&periode_start=2021-01-29&periode_end=2021-01-29&is_reg_del_date_null=1&is_visit_del_date_null=1&offset=0&limit=10&search=&',
+        headers: { 
+            'x-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiTkFNQSAyIiwiaWRfdXNlciI6IjM3MzY4MSIsInJtX251bWJlciI6ImFkbWluIiwicnNfa2V5IjoiQTEyMyIsImlwX2FkZHJlc3MiOiIxMjcuMC4xLjEiLCJhY2Nlc3MiOiJ1c2VyIn0.ubW6fyc7ErYOW2T5qFbjXvLIVTLp05s3A0paQ6wfcmo'
         }
-      : {
-          color: theme.palette.text.primary,
-          backgroundColor: theme.palette.secondary.dark,
-        },
-  title: {
-    flex: '1 1 100%',
-  },
-}));
+    };
+    const getData = async () => {
+        const datas = await axios(config)
+        .then(function (response) {                                
+            console.log(response.data.data)                
+            setData(response.data.data)    
 
-const EnhancedTableToolbar = (props) => {
-  const classes = useToolbarStyles();
-  const { numSelected } = props;
-
-  return (
-    <Toolbar
-      className={clsx(classes.root, {
-        [classes.highlight]: numSelected > 0,
-      })}
-    >
-      {numSelected > 0 ? (
-        <Typography className={classes.title} color="inherit" variant="subtitle1" component="div">
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <Typography className={classes.title} variant="h6" id="tableTitle" component="div">
-          Nutrition
-        </Typography>
-      )}
-
-      {numSelected > 0 ? (
-        <div>
-            delete
-        </div>
-      ) : (
-        <div>
-            filter
-        </div>
-      )}
-    </Toolbar>
-  );
-};
-
-EnhancedTableToolbar.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-};
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-  },
-  paper: {
-    width: '100%',
-    marginBottom: theme.spacing(2),
-  },
-  table: {
-    minWidth: 750,
-  },
-  visuallyHidden: {
-    border: 0,
-    clip: 'rect(0 0 0 0)',
-    height: 1,
-    margin: -1,
-    overflow: 'hidden',
-    padding: 0,
-    position: 'absolute',
-    top: 20,
-    width: 1,
-  },
-}));
-
-export default function EnhancedTable() {
-  const classes = useStyles();
-  const [order, setOrder] = React.useState('asc');
-  const [orderBy, setOrderBy] = React.useState('calories');
-  const [selected, setSelected] = React.useState([]);
-  const [page, setPage] = React.useState(0);
-  const [dense, setDense] = React.useState(false);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
-
-  const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
-    setOrderBy(property);
-  };
-
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelecteds = rows.map((n) => n.name);
-      setSelected(newSelecteds);
-      return;
+        })
+        .catch(function (error) {
+        console.log(error);
+        });                        
     }
-    setSelected([]);
-  };
+        getData();    
+  }, [])
+  
+  const sorting = data.map((value,idx)=>value.data_pasien.umur)
+  const sorting2 = data.map((value,idx)=>value.data_pasien.nama_pasien!==null?value.data_pasien.nama_pasien:'zero')
+  const sorting3 = data.map((value,idx)=>value.data_catatan.icd_9.code!==null?value.data_catatan.icd_9.code:'-')
+  // const sorting4 = sorting3.map((value,idx)=>value!==null?'isi';'null')
+  let arayKosong = []
+  let objekKosong = {}  
+  for(let i=0;i<sorting.length;i++){
+    objekKosong={}
+    objekKosong['makan']=sorting[i]
+    objekKosong['minum']=sorting2[i]
+    objekKosong['medok']=sorting3[i]
+    arayKosong.push(objekKosong)
+  }
 
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected = [];
+  useEffect(()=>
+    console.log(data,arayKosong)    
+  ,[sorting])
 
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
-      );
-    }
 
-    setSelected(newSelected);
-  };
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  const handleChangeDense = (event) => {
-    setDense(event.target.checked);
-  };
-
-  const isSelected = (name) => selected.indexOf(name) !== -1;
-
-  const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
-
-  return (
-    <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <EnhancedTableToolbar numSelected={selected.length} />
-        <TableContainer>
-          <Table
-            className={classes.table}
-            aria-labelledby="tableTitle"
-            size={dense ? 'small' : 'medium'}
-            aria-label="enhanced table"
-          >
-            <EnhancedTableHead
-              classes={classes}
-              numSelected={selected.length}
-              order={order}
-              orderBy={orderBy}
-              onSelectAllClick={handleSelectAllClick}
-              onRequestSort={handleRequestSort}
-              rowCount={rows.length}
+  const additionalCols = [
+    {
+      header: 'Actions',
+      td: (data) => {
+        return (
+          <div>
+            <img
+              src={deleteIcon}
+              alt='delete'
+              width='30'
+              height='20'
+              onClick={() => alert('this is delete for id ' + data.id)}
             />
-            <TableBody>
-              {stableSort(rows, getComparator(order, orderBy))
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row, index) => {
-                  const isItemSelected = isSelected(row.name);
-                  const labelId = `enhanced-table-checkbox-${index}`;
+            <img
+              src={editIcon}
+              alt='edit'
+              width='30'
+              height='20'
+              onClick={() => alert('this is edit for id ' + data.id)}
+            />
+          </div>
+        )
+      }
+    }
+  ]
+  const columns = [
+    {
+      header: 'name',
+      key: 'user.name'
+    },
+    {
+      header: 'Actions',
+      td: (data) => {
+        return (
+          <div>
+            <img
+              src={deleteIcon}
+              alt='delete'
+              width='30'
+              height='20'
+              onClick={() => alert('this is delete for id ' + data.id)}
+            />
+            <img
+              src={editIcon}
+              alt='edit'
+              width='30'
+              height='20'
+              onClick={() => alert('this is edit for id ' + data.id)}
+            />
+          </div>
+        )
+      }
+    }
+  ]
+  const editDownloadProps = (e) => {
+    let newProps = { ...downloadExcelProps }
 
-                  return (
-                    <TableRow
-                      hover
-                      onClick={(event) => handleClick(event, row.name)}
-                      role="checkbox"
-                      aria-checked={isItemSelected}
-                      tabIndex={-1}
-                      key={row.name}
-                      selected={isItemSelected}
-                    >
-                      <TableCell padding="checkbox">
-                        <Checkbox
-                          checked={isItemSelected}
-                          inputProps={{ 'aria-labelledby': labelId }}
-                        />
-                      </TableCell>
-                      <TableCell component="th" id={labelId} scope="row" padding="none">
-                        {row.name}
-                      </TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">{row.protein}</TableCell>
-                    </TableRow>
-                  );
-                })}
-              {emptyRows > 0 && (
-                <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[5, 10, 25]}
-          component="div"
-          count={rows.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onChangePage={handleChangePage}
-          onChangeRowsPerPage={handleChangeRowsPerPage}
-        />
-      </Paper>
-      <FormControlLabel
-        control={<Switch checked={dense} onChange={handleChangeDense} />}
-        label="Dense padding"
+    newProps[e.target.name] = e.target.value
+    setDownloadExcelProps(newProps)
+  }
+  const editDownloadPropsCheckBox = (e) => {
+    let newProps = { ...downloadExcelProps }
+
+    newProps[e.target.name] = e.target.checked
+    setDownloadExcelProps(newProps)
+  }
+  return (
+    <div style={{ margin: '30px' }}>
+      <h2 style={{ textAlign: 'center' }}>react-flexy-table</h2>
+      <div style={{ display: 'flex' }}>
+        <div style={{ margin: '30px' }}>
+          <label>Handle search case sensivite</label>
+          <select
+            onChange={(e) => setCaseSensivite(e.target.value === 'true')}
+            style={{ marginLeft: '10px', padding: '5px' }}
+            value={caseSensivite}
+          >
+            <option value={false}>Close</option>
+            <option value={true}>Open</option>
+          </select>
+        </div>
+        <div style={{ margin: '30px' }}>
+          <label>Handle sortable</label>
+          <select
+            onChange={(e) => setSortable(e.target.value === 'true')}
+            style={{ marginLeft: '10px', padding: '5px' }}
+            value={sortable}
+          >
+            <option value={false}>Close</option>
+            <option value={true}>Open</option>
+          </select>
+        </div>
+        <div style={{ margin: '30px' }}>
+          <label>Handle filterable</label>
+          <select
+            onChange={(e) => setFilterable(e.target.value === 'true')}
+            style={{ marginLeft: '10px', padding: '5px' }}
+            value={filterable}
+          >
+            <option value={false}>Close</option>
+            <option value={true}>Open</option>
+          </select>
+        </div>
+
+        <div style={{ margin: '30px' }}>
+          <label>Download Excel Type</label>
+          <select
+            onChange={editDownloadProps}
+            style={{ marginLeft: '10px', padding: '5px' }}
+            value={downloadExcelProps.type}
+            name='type'
+          >
+            <option value={'filtered'}>Filtered</option>
+            <option value={'paged'}>Paged</option>
+            <option value={'all'}>All</option>
+          </select>
+        </div>
+
+        <div style={{ margin: '30px' }}>
+          <label>Download Excel Show Label</label>
+          <input
+            type='checkbox'
+            onChange={editDownloadPropsCheckBox}
+            style={{ marginLeft: '10px', padding: '5px' }}
+            checked={downloadExcelProps.showLabel}
+            name='showLabel'
+          />
+        </div>
+        <div style={{ margin: '30px' }}>
+          <label>Download Excel title</label>
+          <input
+            type='text'
+            onChange={editDownloadProps}
+            style={{ marginLeft: '10px', padding: '5px' }}
+            value={downloadExcelProps.title}
+            name='title'
+          />
+        </div>
+      </div>
+      <ReactFlexyTable
+        data={arayKosong}
+        sortable={sortable}
+        filterable={filterable}
+        caseSensitive={caseSensivite}
+        additionalCols={additionalCols}
+        globalSearch
+        downloadExcelProps={downloadExcelProps}
+        showExcelButton
       />
     </div>
-  );
+  )
 }
+
+export default PageTwoContent
